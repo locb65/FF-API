ec20d2cd3138465ddf1d31d4c727acbe3d1438f4
locb65 <locb65@gmail.com>
2023-03-14 17:10:13 -0400

    fixed rendering and adding of data to database if not existing

--- App/data/seed.js
+++ App/data/seed.js
@@ -2,26 +2,32 @@
 import Character from "../models/CharacterSchema.js";
 import axios from "axios";
 
+
+
 const fetchCharacter = async() =>{
    const newCharacter = await axios.get("https://www.moogleapi.com/api/v1/characters/random")
    //need conditional to check if character exists in database
-   console.log(Character.find({name: newCharacter.data.name}))
-   if(Character.find({name: newCharacter.data.name}) === null ){
+   console.log(newCharacter.data)
+   const findExisting = await Character.findOne({name: newCharacter.data.name})
+//    console.log(Character.find({name: newCharacter.data.name}))
+   if(findExisting === null ){
 
-   return await Character.create(newCharacter.data);}
-   else {
-    return newCharacter.data;
+   return await Character.create(newCharacter.data);
+} else {
+    return findExisting;
    }
 }
 
-fetchCharacter()
+// fetchCharacter()
+
 
 
 async function seedEmptyCharacters () {
     await Character.deleteMany({})
-    await Character.create({})
+    // await Character.create({})
 }
 
-// seedEmptyCharacters()
+seedEmptyCharacters()
+
 
 export default fetchCharacter
--- App/models/CharacterSchema.js
+++ App/models/CharacterSchema.js
@@ -4,8 +4,15 @@ import mongoose from "../utils/connection.js";
 
 const characterSchema = new mongoose.Schema({
     name: String,
-    age: Number,
+    age: String,
+    gender: String,
+    race: String,
+    job: String,
+    height: String,
+    weight: String,
     origin: String,
+    description: String,
+    // UniqueString:String  
     numberOfMatches: Number, 
     Wins: Number,
     // UniqueString:String
