43e42a7ce7f0e3fd1992d3920cfbd78cb47e514a
locb65 <locb65@gmail.com>
2023-03-13 20:46:54 -0400

    modified planner

---
+++ App/Planning/planner.js
@@ -0,0 +1,29 @@
+ //Step 1:
+ //make connection to mongodb. Done
+
+ //make rudementary schema for characters. Done
+
+ //seed blank databse to mongodb
+ 
+ 
+ //First Get request to create teams       
+        
+        //API that does not have data preloaded
+
+            //need to make character schema to Get Raw JSon of random character
+            //create get request to pull 1 random character
+             //cross check character with DB to see if they exist
+                    //if not seed to database
+                    //if so skip top part
+
+            
+        // then create get request to moogleAPI to pull 6 random characters//JAavscript code split into 2 teams
+
+
+
+
+
+//Step 2: Post request to cast vote on winning team
+
+        //Post request => Make a function to iterate through teams for Each Character number of matches ++
+
