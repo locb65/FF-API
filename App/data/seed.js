710b7734d9a85cfb9993315e4f1432119bc49b34
Loc Bui <buda@Locs-MacBook-Pro.local>
2023-03-14 12:07:53 -0400

    fixed seeding of character

--- App/data/seed.js
+++ App/data/seed.js
@@ -5,6 +5,7 @@ import axios from "axios";
 const fetchCharacter = async() =>{
    const newCharacter = await axios.get("https://www.moogleapi.com/api/v1/characters/random")
    console.log(newCharacter)
+   await Character.create(newCharacter.data)
 }
 
 fetchCharacter()
@@ -15,6 +16,6 @@ async function seedEmptyCharacters () {
     await Character.create({})
 }
 
-seedEmptyCharacters()
+// seedEmptyCharacters()
 
 export default fetchCharacter
