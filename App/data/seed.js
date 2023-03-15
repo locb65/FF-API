44cac09599b9a32c9b51a0d1e63d6a048cb858b3
locb65 <locb65@gmail.com>
2023-03-15 13:08:50 -0400

    attempt to make a team, failed

--- App/data/seed.js
+++ App/data/seed.js
@@ -5,19 +5,34 @@ import axios from "axios";
 
 
 const fetchCharacter = async() =>{
-   const newCharacter = await axios.get("https://www.moogleapi.com/api/v1/characters/random")
-   //need conditional to check if character exists in database
-   console.log(newCharacter.data)
-   const findExisting = await Character.findOne({name: newCharacter.data.name})
-//    console.log(Character.find({name: newCharacter.data.name}))
-   if(findExisting === null ){
+   
+   const team = []
+   let maxTeamNumber = 3
+   //for loop to make 3 random characters into a team
+   for (let i =0; i <= maxTeamNumber; i++){
 
-   return await Character.create(newCharacter.data);
-} else {
-    return findExisting;
+         const newCharacter = await axios.get("https://www.moogleapi.com/api/v1/characters/random")
+
+         const createCharacter = Character.create(newCharacter.data);
+
+         //need conditional to check if character exists in database
+         console.log(newCharacter.data)
+
+         const findExisting = await Character.findOne({name: newCharacter.data.name})
+
+      //    console.log(Character.find({name: newCharacter.data.name}))
+
+         if(findExisting === null ){
+
+
+            team.push(createCharacter)
+            console.log(team)
+            return team
+         }
+      return findExisting;
    }
+   
 }
-
 // fetchCharacter()
 
 
